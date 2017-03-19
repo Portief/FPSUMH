f14a3f081b178dac7dcbf2a4d3674e4df0159b7d
Javi <jverdugar@gmail.com>
2017-03-19 21:14:47 +0100

    Cambios en el matchmaking, ya debería funcionar

---
+++ Assets/Scripts/Lobby/HostGame.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class HostGame : MonoBehaviour
+{
+    [SerializeField]
+    private uint roomSize = 10;
+
+    private string roomName;
+
+    public NetworkManager networkManager;
+
+    void Start()
+    {
+        
+        if (networkManager.matchMaker == null)
+            networkManager.StartMatchMaker();
+    }
+
+    public void setRoomName (string name)
+    {
+        roomName = name;
+    }
+
+    public void CreateRoom ()
+    {
+        if(roomName != "" && roomName != null)
+        {
+            networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+        }
+    }
+
+}
